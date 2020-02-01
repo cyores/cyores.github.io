dc6f9913f1823f44b82327fe07c718187dcd5893
Christian Yores <christianyores@gmail.com>
2020-02-01 18:27:10 -0500

    Header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import MadeonImg from "../images/madeon270.jpg";
 
 // components
 import Flex from "./utils/Flex";
+
+const Wrapper = styled.div`
+    background: url(${MadeonImg});
+    background-size: cover;
+    background-repeat: no-repeat;
+    margin-bottom: var(--space-lg);
+`;
+
+const DesktopLayout = styled.div`
+    display: block;
+    background: radial-gradient(
+        circle,
+        rgba(255, 255, 255, 0.33) 0%,
+        rgba(255, 255, 255, 0) 80%
+    );
+    @media only screen and (max-width: 625px) {
+        display: none;
+    }
+`;
+
+const PhoneLayout = styled.div`
+    display: none;
+
+    @media only screen and (max-width: 625px) {
+        display: block;
+    }
+`;
+
+const SlideOpen = styled.div`
+    @keyframes slideOpen {
+        0% {
+            height: 0;
+            opacity: 0;
+        }
+        100% {
+            height: 160px;
+            opacity: 1;
+        }
+    }
+
+    animation-name: slideOpen;
+    animation-duration: 250ms;
+    animation-timing-function: ease-in-out;
+`;
+
+const SlideClosed = styled.div`
+    height: 0;
+    opacity: 0;
+    pointer-events: none;
+    @keyframes slideClosed {
+        0% {
+            height: 160px;
+            opacity: 1;
+            display: block;
+        }
+        100% {
+            height: 0;
+            opacity: 0;
+            display: none;
+        }
+    }
+
+    animation-name: slideClosed;
+    animation-duration: 250ms;
+    animation-timing-function: ease-in-out;
+`;
+
+const MenuButton = styled.span`
+    display: none;
+    font-size: var(--text-base-size);
+    color: var(--color-bg);
+    padding: var(--space-sm) var(--space-xxs);
+
+    @media only screen and (max-width: 625px) {
+        display: block;
+    }
+`;
 
 const NavItem = styled.div`
     display: inline-block;
     cursor: pointer;
     font-size: var(--text-md);
+    color: var(--color-bg);
     line-height: 0.9;
     opacity: 0.8;
     border-bottom: 3px solid transparent;
@@ -17,31 +96,73 @@ const NavItem = styled.div`
         transform: translate3d(0, -1px, 0);
         border-bottom: 3px solid var(--color-primary);
     }
+
+    @media only screen and (max-width: 625px) {
+        display: block;
+        text-align: center;
+        margin: var(--space-xs) 0;
+    }
 `;
 
 const Spacer = styled.div`
     display: inline-block;
     padding: var(--space-md);
-    color: var(--color-complement);
+    color: var(--color-primary);
+    font-size: var(--text-md);
+
+    @media only screen and (max-width: 625px) {
+        display: none;
+    }
 `;
 
-const Header = () => (
-    <>
-        <Flex>
-            <div>
-                <NavItem>home</NavItem>
-                <Spacer>/</Spacer>
-                <NavItem>education</NavItem>
-                <Spacer>/</Spacer>
-                <NavItem>experience</NavItem>
-                <Spacer>/</Spacer>
-                <NavItem>projects</NavItem>
-                <Spacer>/</Spacer>
-                <NavItem>contact</NavItem>
-            </div>
-        </Flex>
-        <br></br>
-    </>
+const MenuOptions = () => (
+    <Flex>
+        <div>
+            <NavItem>home</NavItem>
+            <Spacer>/</Spacer>
+            <NavItem>education</NavItem>
+            <Spacer>/</Spacer>
+            <NavItem>experience</NavItem>
+            <Spacer>/</Spacer>
+            <NavItem>projects</NavItem>
+            <Spacer>/</Spacer>
+            <NavItem>contact</NavItem>
+        </div>
+    </Flex>
 );
+
+const Header = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <Wrapper>
+            <MenuButton
+                onClick={() => {
+                    const currOpen = open;
+                    setOpen(!currOpen);
+                }}
+            >
+                {open ? <span>close</span> : <span>menu</span>}
+            </MenuButton>
+
+            {open ? (
+                <SlideOpen>
+                    <PhoneLayout open={open}>
+                        <MenuOptions />
+                    </PhoneLayout>
+                </SlideOpen>
+            ) : (
+                <SlideClosed>
+                    <PhoneLayout open={open}>
+                        <MenuOptions />
+                    </PhoneLayout>
+                </SlideClosed>
+            )}
+
+            <DesktopLayout>
+                <MenuOptions />
+            </DesktopLayout>
+        </Wrapper>
+    );
+};
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql } from "gatsby";
 
 // components
 import Header from "../components/Header";
-import Madeon from "../components/utils/Madeon";
 import Flex from "../components/utils/Flex";
 
 const Index = () => {
@@ -22,19 +21,6 @@ const Index = () => {
     return (
         <>
             <Header />
-            <Flex>
-                <Madeon
-                    // style={{ width: "400px", clipPath: "polygon(50% 0, 100% 50%, 50% 100%, 0 50%)" }}
-                    style={{
-                        width: "16%",
-                        overflowY: "hidden",
-                        // transform: "rotate(90deg) translateX(-500px)",
-                        marginBottom: "-200px",
-                        clipPath: "polygon(50% 0, 100% 50%, 50% 50%, 0 50%)"
-                    }}
-                    durationFadeIn={1500}
-                />
-            </Flex>
 
             <Flex>
                 <h1>{data.site.siteMetadata.title}</h1>
